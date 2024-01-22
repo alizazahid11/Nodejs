0d29501356d32486262021db403e861fdc9f2d9a
aliza zahid <alizayzah11@gmail.com>
2024-01-22 15:15:43 -0800

    fixing cors issue

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express=require('express');
+const cors=require('cors');
 require('./db/config')
 const app=express();
 app.use(express.json());//middleware
+const corsOptions = {
+    origin: 'http://localhost:3000',  // Replace with your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+
 const User = require('./db/User')
 app.post("/register",async(req,res)=>{
     //steps to show api in mongodb
